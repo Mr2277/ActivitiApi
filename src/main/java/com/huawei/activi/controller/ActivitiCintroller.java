8e9cad34f5f38c53f74e870999999c3dfb4c5dce
sun <837544713@qq.com>
2020-10-02 10:39:21 +0800

    add deployment

--- src/main/java/com/huawei/activi/controller/ActivitiCintroller.java
+++ src/main/java/com/huawei/activi/controller/ActivitiCintroller.java
@@ -1,8 +1,10 @@
 package com.huawei.activi.controller;
 
 import org.activiti.engine.*;
+import org.activiti.engine.repository.Deployment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -39,7 +41,19 @@ public class ActivitiCintroller {
     }
 
     @RequestMapping("/deloy")
-    public void deloy(String fileBpmn) {
+    public void deloy(@RequestParam("fileBpmn") String fileBpmnName) {
+        String preFilePath = "processes/";
+        String fileBpmn = preFilePath + fileBpmnName + ".bpmn20.xml";
+        System.out.println(fileBpmnName);
+        Deployment deployment = repositoryService.createDeployment()//创建一个部署对象
+                   .name(fileBpmnName)//添加部署的名称
+                   .key(fileBpmnName)
+                   .addInputStream(fileBpmn, this.getClass().getClassLoader().getResourceAsStream(fileBpmn))
+//                .addInputStream(png, this.getClass().getClassLoader().getResourceAsStream(png))
+                   .deploy();//完成部署
 
+
+        System.out.println(deployment.toString());
+        System.out.println("Id:" + deployment.getId() + "  Name:" + deployment.getName() + "  key:" + deployment.getKey());
     }
 }
